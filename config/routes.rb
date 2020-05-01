02f84954dc2bb7d1ee8fe842eda3750858026036
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-05-01 10:08:46 +0100

    redux-promise-middleware working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
+  devise_for :users, skip: [:sessions]
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  root to: 'application#redirect_to_dashboard'
+  get '/dashboard', to: 'pages#dashboard'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
