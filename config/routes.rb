ed3162f1577f427ecfd0c65f7f786b8b3d941aac
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-05-28 18:00:16 +0100

    Suggested Rails changes by running app:update command

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions]
-  devise_scope :user do
-    get 'login', to: 'devise/sessions#new', as: :new_user_session
-    post 'login', to: 'devise/sessions#create', as: :user_session
-    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
-
-  root to: 'application#redirect_to_dashboard'
-  get '/dashboard', to: 'pages#dashboard'
-
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      get 'tagged_posts/:instagram_username', to: 'tagged_posts#index'
-      post 'tagged_posts/:instagram_username', to: 'tagged_posts#create'
-      patch 'tagged_posts/:instagram_username', to: 'tagged_posts#update'
-      delete 'tagged_posts/:instagram_username', to: 'tagged_posts#delete'
-    end
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
