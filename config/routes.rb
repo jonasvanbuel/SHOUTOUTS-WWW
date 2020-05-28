946ff2619dd7c35513a2999c325e52a0dff2bcbe
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-05-28 18:22:43 +0100

    Reinstated routes + few other default settings from LW template

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:sessions]    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  root to: 'application#redirect_to_dashboard'
+  get '/dashboard', to: 'pages#dashboard'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get 'tagged_posts/:instagram_username', to: 'tagged_posts#index'
+      post 'tagged_posts/:instagram_username', to: 'tagged_posts#create'
+      patch 'tagged_posts/:instagram_username', to: 'tagged_posts#update'
+      delete 'tagged_posts/:instagram_username', to: 'tagged_posts#delete'
+    end
+  end
 end
