c9bb31e43e043d0bd2f3a97364b5d023dcd26997
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-07-16 16:18:30 +0100

    filterBar review + finetuning w/ variables

--- app/javascript/app/components/menu.jsx
+++ app/javascript/app/components/menu.jsx
@@ -4,19 +4,21 @@ import { connect } from 'react-redux';
 
 class Menu extends Component {
   componentDidMount() {
-    const filterBar = document.getElementsByClassName('filter-bar')[0];
-    const startLivestreamBtn = document.getElementsByClassName('start-livestream')[0];
-    const connectedAccountsTitle = document.getElementsByClassName('connected-accounts-title')[0];
-    const margin = filterBar.offsetHeight - startLivestreamBtn.offsetHeight;
-    connectedAccountsTitle.style.marginTop = `${margin}px`;
+    // const filterBar = document.getElementsByClassName('filter-bar')[0];
+    // const startLivestreamBtn = document.getElementsByClassName('start-livestream')[0];
+    // const connectedAccountsTitle = document.getElementsByClassName('connected-accounts-title')[0];
+    // const margin = filterBar.offsetHeight - startLivestreamBtn.offsetHeight;
+    // connectedAccountsTitle.style.marginTop = `${margin}px`;
   }
 
   render() {
     return (
       <div className="menu">
-        <a href="/live" target="_blank" className="start-livestream">
-          start livestream
-        </a>
+        <div className="submenu-top">
+          <a href="/live" target="_blank" className="start-livestream">
+            start livestream
+          </a>
+        </div>
         <div className="connected-accounts-title">
           connected accounts:
         </div>
