f8858b88b742d6a71ccf5b78f52d8fc4dd7b534f
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-07-30 16:15:44 +0100

    view-all-posts button in place to clear search filter

--- app/javascript/app/components/post_list_container.jsx
+++ app/javascript/app/components/post_list_container.jsx
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 // COMPONENTS
 import FilterBar from '../components/filter_bar';
 import PostList from '../components/post_list';
 
+// ACTIONS
+import { setFiltered, setFilter } from '../actions';
+
+
 class PostListContainer extends Component {
+  componentDidMount() {
+    const viewAllPosts = document.getElementsByClassName('view-all-posts')[0];
+    const label = viewAllPosts.getElementsByClassName('symbol-label')[0];
+    const symbol = label.parentElement.getElementsByClassName('fas')[0];
+
+    symbol.addEventListener('mouseenter', () => {
+      if (label.classList.contains('invisible')) {
+        label.classList.remove('invisible');
+      }
+    });
+    symbol.addEventListener('mouseleave', () => {
+      if (!label.classList.contains('invisible')) {
+        label.classList.add('invisible');
+      }
+    });
+  }
+
+  handleClick = () => {
+    const { setFiltered, setFilter } = this.props;
+    setFiltered('');
+    setFilter('');
+
+  }
+
   render() {
+    const { filtered } = this.props;
+
     return (
       <div className="post-list-container">
         <FilterBar />
         <PostList />
+        <div className={`view-all-posts${filtered === true ? '' : ' invisible'}`}>
+          <i className="fas fa-plus-circle" onClick={this.handleClick}></i>
+          <span className="symbol-label invisible">view all posts</span>
+        </div>
       </div>
     );
   }
 }
 
-export default PostListContainer;
+function mapStateToProps(state) {
+  return {
+    filtered: state.filtered,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ setFiltered, setFilter }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostListContainer);
