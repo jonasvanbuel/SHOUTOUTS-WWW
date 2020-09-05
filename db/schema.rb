dc19e376c4c86cacfa744e71e89687f7245e8989
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-09-05 17:41:39 +0100

    hashtag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_162531) do
+ActiveRecord::Schema.define(version: 2020_09_05_163835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "instagram_accounts", force: :cascade do |t|
     t.string "username"
