d344bf2266e68cf6e2d550fbe6ba614d9dd35e37
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-11-05 17:40:00 +0100

    Logo pattern animation hard refresh every time

--- app/javascript/app/components/logo-iframe/logo-iframe.jsx
+++ app/javascript/app/components/logo-iframe/logo-iframe.jsx
@@ -7,25 +7,35 @@ import Foreground from 'images/logo_w_shadows/logo_fg_4.png';
 import Pattern from 'images/logo_v2_bg.svg';
 
 class LogoIframe extends Component {
+  componentDidMount() {
+    // To make sure animation gets reloaded everytime, and not cached
+    const iframeEl = document.getElementById('logo-iframe');
+    iframeEl.contentWindow.location.reload(true);
+  }
+
   renderScript() {
     return (
       `<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.0/anime.min.js" integrity="sha512-LfB+BcvR3zBt7ebVskhSWiSbEUiG3p5EcCprkieldsKhBeR6wpnLi0VpWC2GNgVGWP2n/skO8Bx2oKNjUhXCkw==" crossorigin="anonymous"></script>
       <script>
-        anime({
-          targets: '.logo-pattern',
-          translateX: 100,
-          duration: 30000,
-          loop: true,
-          direction: 'alternate',
-          easing: 'linear'
-        });
+        const logoPatternAnimation = () => {
+          console.log('logoPatternAnimation triggered...');
+          anime({
+            targets: '.logo-pattern',
+            translateX: 100,
+            duration: 30000,
+            loop: true,
+            direction: 'alternate',
+            easing: 'linear'
+          });
+        }
+        logoPatternAnimation();
       </script>`
     )
   }
 
   render() {
     // const script = this.renderScript();
-    const initialContent = `<!DOCTYPE html><html><head>${document.head.innerHTML}</head><body style="background-color: white"><div></div>${this.renderScript()}</body></html>`;
+    const initialContent = `<!DOCTYPE html><html><head>${document.head.innerHTML}<meta http-Equiv="Cache-Control" Content="no-cache" /><meta http-Equiv="Pragma" Content="no-cache" /><meta http-Equiv="Expires" Content="0" /></head><body style="background-color: white"><div></div>${this.renderScript()}</body></html>`;
 
     return (
       <Frame initialContent={initialContent} contentDidMount={this.renderScript} id="logo-iframe" scrolling="no" >
